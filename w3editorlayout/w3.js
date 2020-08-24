27d85870df1a11e729a8a36274f13ec9fa69ebef
pritam kumari <pritamchaudhary96@gmail.com>
2020-08-24 20:21:14 +0530

    initial commit

---
+++ w3editorlayout/w3.js
@@ -0,0 +1,39 @@
+function closebutton()
+{
+    alert("Thanks for visiting");
+}
+
+function infobutton()
+{
+    alert("click to know more");
+}
+
+function myhome()
+{
+    alert("Welcome to home page");
+} 
+
+function mymenu()
+{
+    alert("open menu");
+} 
+
+function save()
+{
+    alert("Welcome to home page");
+} 
+
+function rotation()
+{
+    alert("rotate page");
+} 
+
+function changeTheme()
+{
+    alert("changed page color");
+} 
+
+function run()
+{
+    alert("final result");
+}
